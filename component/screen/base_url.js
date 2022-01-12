6b6f5ea194a92cc9feddd17708c8c15675a985f2
DonBosco Mushi <donnicholaus@gmail.com>
2022-01-12 18:25:48 +0300

    fix package conflicts

---
+++ component/screen/base_url.js
@@ -0,0 +1,3 @@
+export default base_url = 'http://bus-ticket.bdtask.com/bus_demo_v5/'
+// export default base_url = 'https://app.artanitravel.com/'
+// export default base_url = 'http://ubcompartilhado.com.br/'
